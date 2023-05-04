8310639dea04ee3ddf639c85dd2fedd9d008ddb8
Ashmit Ranjan <ashmitranjan978@gmail.com>
2023-05-04 20:42:40 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
    const [backend,setBackendData] = useState({names:["efe"],rates:[4.3],reviews:["jenf"]})
 
    useEffect(()=>{
-      fetch("https://88ad-3-109-113-184.ngrok-free.app/data")
+      fetch("https://950c-3-109-113-184.ngrok-free.app/data")
       .then(
          response=>response.json()
       )
